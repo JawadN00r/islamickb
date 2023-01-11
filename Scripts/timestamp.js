2bd11008c149e48b30592978337add28c411ca8c
JawadNoor <jawad.asif.bd@gmail.com>
2023-01-11 15:41:17 +0600

    test

--- Scripts/timestamp.js
+++
@@ -1,9 +0,0 @@
-function my_function(timestamp) {
-  var aud = document.getElementById("myAudio");
-  self.addEventListener("click", function () {
-    aud.currentTime = timestamp;
-    aud.play();
-  });
-  return;
-}
-module.exports = my_function;
